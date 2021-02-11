a894c20d3517db32b9dbc228c735cafa0e8ee2fa
Gauravpurohit409 <gauravpurohit6666@gmail.com>
2021-02-11 19:32:01 +0530

    change the manner

--- src/components/App.js
+++ src/components/App.js
@@ -7,45 +7,52 @@ const App = () => {
 
     const [renderBall,setRenderBall] = useState(false);
     const [posi,setPosi] = useState(0);
-    const [ballPosition,setBallPosition] = useState({left:5,top:0});
+    const [ballPosition,setBallPosition] = useState({left:0,top:0});
     
     function move(l,t){
-           setBallPosition((prevstate) => {
-            // console.log("here i am",ballPosition.left,state.left);
-            ballPosition.left =prevstate.left +l;
-            ballPosition.top = prevstate.top +t;
-            return ballPosition;
-        });
-        //    console.log(posi,ballPosition.left,ballPosition.top);
-        setPosi({
-            pos : posi+1,
+        //    setBallPosition((prevstate) => {
+        //     // console.log("here i am",ballPosition.left,state.left);
+        //     ballPosition.left =prevstate.left +l;
+        //     ballPosition.top = prevstate.top +t;
+        //     return ballPosition;
+        // });
+        setBallPosition({
+            left :ballPosition.left +l,
+            top:ballPosition.top + t,
         })
+            // console.log(posi,ballPosition.left,ballPosition.top);
+        // setPosi({
+        //     pos : posi+1,
+        // })
     }
+
+    const handleListener =  (event)=>{
+        // alert("hello");
+        switch(event.keyCode){
+            case 37 :
+                move(-5,0);
+                //  console.log(ballPosition);
+            break;
+            case 38 :
+                move(0,-5);
+                // console.log(ballPosition);
+
+            break;
+            case 39 :
+                move(5,0);
+                // console.log(ballPosition);
+
+            break;
+            case 40 :
+                move(0,5);
+            //     console.log(ballPosition);
+            break;
+        }
+    };
     // bind ArrowRight keydown event
     useEffect(()=> {
-        document.addEventListener("keydown",(event)=>{
-            switch(event.keyCode){
-                case 37 :
-                    move(-5,0);
-                    //  console.log(ballPosition);
-                break;
-                case 38 :
-                    move(0,-5);
-                    // console.log(ballPosition);
-
-                break;
-                case 39 :
-                    move(5,0);
-                    // console.log(ballPosition);
-
-                break;
-                case 40 :
-                    move(0,5);
-                //     console.log(ballPosition);
-                break;
-            }
-        });
-
+        document.addEventListener("keydown",handleListener);
+        return () => document.removeEventListener("keydown",handleListener);
     },[ballPosition]);
   
     const buttonClickHandler = () => {
